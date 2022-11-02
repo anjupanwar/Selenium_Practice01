fa272b5ad718158cc30d3ec7f0521cb1d763b5d8
anjupanwar <anjupanwar23@gmail.com>
2022-11-02 12:40:07 +0530

    This is a second update from my side

--- src/test/java/cmddemo/Webdrivermanager.java
+++ src/test/java/cmddemo/Webdrivermanager.java
@@ -1,14 +1,27 @@
 package cmddemo;
 
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
 import master.Initialization;
 
 public class Webdrivermanager extends Initialization {
 
+
+	
 	public static void main(String[] args) throws Exception {
 		Initialization in = new Initialization();
 		in.init();
 		in.browser();
-
-	}
+		in.driver.manage().window().maximize();
+		in.driver.manage().window().fullscreen();
+		in.driver.get(in.pro.getProperty("url"));
+		in.driver.findElement(By.id("UserName")).sendKeys("hms@gmail.com");
+		in.driver.findElement(By.id("Password")).sendKeys("Admin$123");
+		in.driver.findElement(By.id("Password")).sendKeys(Keys.ENTER);
+}
+	
 
 }
